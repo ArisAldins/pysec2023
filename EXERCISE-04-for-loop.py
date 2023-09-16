96b25f770c5e70b5477fcae04d7f53699f25e531
ArisAldins <144523090+ArisAldins@users.noreply.github.com>
2023-09-16 13:00:45 +0300

    Delete EXERCISE-04-for-loop.py

--- EXERCISE-04-for-loop.py
+++
@@ -1,8 +0,0 @@
-cars = ["Ford", "MB", "GAZ", "Tesla", "VAZ"]
-
-for car in cars:
-    if car == "VAZ":
-        print("Mēs atradām VAZ :) !!!")
-        break  # iziet, ja atrasts VAZ
-else:
-    print("Diemžēl neatradām VAZ :( ") #paziņojums, ja nav atrasts VAZ
